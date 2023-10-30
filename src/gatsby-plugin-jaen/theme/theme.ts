a61c2d035fe36f9dbda6b00cf607ec2559aeb372
Nico Schett <nicoschett@icloud.com>
2023-10-30 15:30:50 +0100

    feat(theme): update brand color
    - Updated the brand color in the theme file to use the baseTheme's purple color.

--- src/gatsby-plugin-jaen/theme/theme.ts
+++ src/gatsby-plugin-jaen/theme/theme.ts
@@ -5,25 +5,12 @@ import {
   baseTheme
 } from '@chakra-ui/react'
 
-export const colors = {}
-
 const theme = extendTheme(
   baseTheme,
   {
     colors: {
       ...baseTheme.colors,
-      brand: {
-        100: '#e9f3fc',
-        200: '#bfdaf4',
-        300: '#67a7e2',
-        400: '#509add',
-        500: '#348dd7',
-        600: '#0580d2',
-        700: '#046eb6',
-        750: '#035d9b',
-        800: '#035d9b',
-        900: '#013d68'
-      }
+      brand: baseTheme.colors.purple
     }
   },
   withDefaultColorScheme({
